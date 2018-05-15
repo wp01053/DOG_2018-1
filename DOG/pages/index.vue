3c25037926bc121c2ffc763bcf64e711ac64b285
jaeyeoupjung <wp01053@naver.com>
2018-05-15 09:50:54 +0900

    first commit

---
+++ DOG/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+  <div class="content">
+  <h1 class="title">개사랑</h1>
+  <h2 class="subtitle">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>
+   </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
